2ca8ca803a2d5b93d2d2b9dbc24533ff22241333
Cecily Grossmann <grssmnn2@gmail.com>
2017-12-21 13:58:55 -0600

    got giphs to show up

--- app.js
+++ app.js
@@ -1,49 +1,86 @@
-// make initial array of buttons for user to choose from
-var animals = ["Dog", "Cat", "Elephant", "Gorilla", "Giraffe", "Penguin", "Porcupine", "Rabbit"];
+$(document).ready(function () {
+    // make initial array of buttons for user to choose from
+    var animals = ["Dog", "Cat", "Elephant", "Gorilla", "Giraffe", "Penguin", "Porcupine", "Rabbit"];
 
-function giphy() {
-    var animal = $(this).attr("data-name");
-    // function to pull JSON data from Giphy API showing PG-13 and below with limit of 10
-    var queryURL = "https://api.giphy.com/v1/gifs/trending?t=" + animal + "&api_key=DkTJLa4KpLC7I9SSyyOGt6AqDq1dyaGw&limit=10&rating=PG-13";
+    // pull data using ajax, reference using response.data
+    function displayGiphy() {
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function (data) {
-        console.log(data);
-    });
-}
-// make a function that takes user input and adds button/also searchable
-function addButton() {
-    // this keeps every button from adding over and over when search is pressed
-    $(".buttons").empty();
+        var animal = $(this).attr("data-name");
 
-    // make for loop to create button for each member of array and put into buttons div
-    for (var i = 0; i < animals.length; i++) {
-        $(".buttons").append("<button>" + animals[i] + "</button>");
-        $("<button>").attr("data-name", animals[i]);
+        // url to pull JSON data from Giphy API showing PG-13 and below with limit of 10
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=DkTJLa4KpLC7I9SSyyOGt6AqDq1dyaGw&q=" + animal + "&limit=10&offset=0&rating=PG-13&lang=en";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            for (var j = 0; j < response.data.length; j++) {
+                var animalDiv = $("<div class = 'animal'>");
+                var rating = response.data[j].rating;
+                var ratingParagraph = $("<p>").html("Rating:" + rating);
+                animalDiv.append(ratingParagraph);
+                var giphyImg = response.data[j].images.fixed_height_still.url;
+                var image = $("<img>").attr("src", giphyImg);
+                animalDiv.append(image);
+                // send animalDiv to giphyImages Div on html page
+                $(".giphyImages").prepend(animalDiv);
+            }
+        });
+
     }
 
-}
+    // make a function that takes user input and adds button/also searchable
+    function addButton() {
+        // this keeps every button from adding over and over when search is pressed
+        $(".buttons").empty();
 
-$("#add-animal").on("click", function (event) {
-    event.preventDefault();
+        // make for loop to create button for each member of array and put into buttons div
+        for (var i = 0; i < animals.length; i++) {
+            var b = $("<button>");
+            b.attr("data-name", animals[i]);
+            b.addClass("animal");
+            b.text(animals[i]);
+            $(".buttons").append(b);
+        }
 
-    // variable animal is whatever what entered by user in text box
-    var animal = $("#animal-input").val().trim();
-    // push animal to the array
-    animals.push(animal);
-    // Calling renderButtons which handles the processing of our movie array
+    }
+    // when search button is clicked, add that animal to the array in button format
+    $("#add-animal").on("click", function (event) {
+        event.preventDefault();
+
+        // variable animal is whatever what entered by user in text box
+        var animal = $("#animal-input").val().trim();
+        // push animal to the array
+        animals.push(animal);
+        //   run addButton function to create button for each animal user typed in search box
+        addButton();
+
+
+    });
+    // new elements gain features of preexisting buttons
+    $(document).on("click", ".animal", displayGiphy);
+
     addButton();
 
 });
 
-addButton();
 
 
 
 
 
+// function gotData(giphy) {
+//     for (var j = 0; j < giphy.data.length; j++) {
+//         $("<img>").append(giphy.data[j].url);
+//         $(".giphyImages").append("<img>");
+
+//     }
+
+     // when button is clicked, pull GIPHY data and add to giphy div
+    //  $(this).on("click", function () {
+    //     setup();
+    //     gotData();
+    // });
 
 // make sure 10 image show up with search, not moving
 // if user clicks an image, it begins moving, user clicks again and image stops moving
